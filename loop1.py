e333edff65f04fa9bdbff5f6e1f8df44f99c6143
Adicurry3000 <97518508+Adicurry3000@users.noreply.github.com>
2022-01-11 17:01:36 +0530

    Add files via upload
    You can find all my beginner projects here

---
+++ loop1.py
@@ -0,0 +1,12 @@
+#for i in range(1,11):
+ #   print(i,'hello',sep='.',end=' ')
+    
+'''for i in range(0,51,2):
+    print(i)
+    if (i==30):
+        break'''
+
+a = int(input('enater a number'))       
+for i in range(1,a):
+    a = a*i
+print(a)
